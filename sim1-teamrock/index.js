61f5785298ef38d5daa8d75b104d34692271c9d1
VIISilver <jparke71@hotmail.com>
2017-09-22 16:31:15 -0600

    All backend is working on postman, but does not communicate with fromt

--- sim1-teamrock/index.js
+++ sim1-teamrock/index.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const massive = require('massive');
-require('dotenv').config()
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const massive = require('massive');
+// require('dotenv').config()
 
 
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
-massive(process.env.CONNECTION_STRING).then(db => app.set('db', db));
-const controllers = require('./server/controllers');
-const baseURL = '/api/products'
+// const app = express();
+// app.use(bodyParser.json());
+// app.use(cors());
+// massive(process.env.CONNECTION_STRING).then(db => app.set('db', db));
+// const controllers = require('./server/controllers');
+// const baseURL = '/api/products'
 
-app.post(baseURL, controllers.createItem);
-app.get(baseURL + '/:id', controllers.selectItem);
-app.put(baseURL + '/:id', controllers.editItem);
-app.delete(baseURL + '/:id', controllers.deleteItem);
+// app.post(baseURL, controllers.createItem);
+// app.get(baseURL + '/:id', controllers.selectItem);
+// app.put(baseURL + '/:id', controllers.editItem);
+// app.delete(baseURL + '/:id', controllers.deleteItem);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => { console.log('Serverlistening on port ' + port + '.'); } );
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => { console.log('Serverlistening on port ' + port + '.'); } );
--- sim1-teamrock/server/controllers.js
+++ sim1-teamrock/server/controllers.js
@@ -1,38 +1,36 @@
-module.exports = {
-    createItem: (req, res, next) => {
-        const db = req.app.get('db');
-        const { name, price } = req.body
+// module.exports = {
+//     createItem: (req, res, next) => {
+//         const db = req.app.get('db');
+//         const { name, price } = req.body
 
-        db.create_product([name, price])
-        .then( () => res.status(200).send() )
-        .catch( () => res.status(500).send() );
-    },
+//         db.create_product([name, price])
+//         .then( () => res.status(200).send() )
+//         .catch( () => res.status(500).send() );
+//     },
 
-    selectItem: (req, res, next) => {
-        const db = req.app.get('db');
-        const { params } = req;
+//     selectItem: (req, res, next) => {
+//         const db = req.app.get('db');
+//         const { params } = req;
 
-        db.read_product([params.id])
-        .then( (product) => res.status(200).send(product) )
-        .catch( () => res.status(500).send() );
-    },
+//         db.read_product([params.id])
+//         .then( (product) => res.status(200).send(product) )
+//         .catch( () => res.status(500).send() );
+//     },
 
-    editItem: (req, res, next) => {
-        const db = req.app.get('db');
-        const { params, query } = req;
+//     editItem: (req, res, next) => {
+//         const db = req.app.get('db');
 
-        db.update_product([ params.id, query.desc ])
-        .then( () => res.status(200).send() )
-        .catch( () => res.status(500).send() );
-    },
+//         db.update_product( req.params.id, req.body.name, req.body.price)
+//         .then( () => res.status(200).send() )
+//         .catch( () => res.status(500).send() );
+//     },
 
-    deleteItem: (req, res, next) => {
-        const db = req.app.get('db');
-        const { params } = req;
-        console.log(params.id);
+//     deleteItem: (req, res, next) => {
+//         const db = req.app.get('db');
+//         const { params } = req;
 
-        db.delete_product(params.id)
-        .then( () => res.status(200).send() )
-        .catch( () => res.status(500).send() );
-    }
-}
+//         db.delete_product(params.id)
+//         .then( () => res.status(200).send() )
+//         .catch( () => res.status(500).send() );
+//     }
+// }
