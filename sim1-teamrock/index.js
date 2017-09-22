c2593d9d84105177fafe0a4791b94987bd527ae4
VIISilver <jparke71@hotmail.com>
2017-09-22 15:29:28 -0600

    Post, delete, and update are not working

--- sim1-teamrock/index.js
+++ sim1-teamrock/index.js
@@ -10,12 +10,12 @@ app.use(bodyParser.json());
 app.use(cors());
 massive(process.env.CONNECTION_STRING).then(db => app.set('db', db));
 const controllers = require('./server/controllers');
-const baseURL = '/api/products';
+const baseURL = '/api/products'
 
-app.post(baseURL, controllers.createItem);
-app.get(baseURL, controllers.selectItem);
-app.put(baseURL, controllers.editItem);
-app.delete(baseURL, controllers.deleteItem);
+// app.post(baseURL, controllers.createItem);
+// app.get(baseURL + '/:id', controllers.selectItem);
+// app.put(baseURL + '/:id', controllers.editItem);
+// app.delete(baseURL + '/:id', controllers.deleteItem);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => { console.log('Serverlistening on port ' + port + '.'); } );
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => { console.log('Serverlistening on port ' + port + '.'); } );
