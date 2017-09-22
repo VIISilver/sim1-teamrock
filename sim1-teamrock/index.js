4f849117640f4a32bd9b35477529156c8392ccc3
VIISilver <jparke71@hotmail.com>
2017-09-22 14:38:29 -0600

    nodemon is running the backend without problems

---
+++ sim1-teamrock/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const massive = require('massive');
+require('dotenv').config()
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+massive(process.env.CONNECTION_STRING).then(db => app.set('db', db));
+const controllers = require('./server/controllers');
+const baseURL = '/api/products';
+
+app.post(baseURL, controllers.createItem);
+app.get(baseURL, controllers.selectItem);
+app.put(baseURL, controllers.editItem);
+app.delete(baseURL, controllers.deleteItem);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => { console.log('Serverlistening on port ' + port + '.'); } );
