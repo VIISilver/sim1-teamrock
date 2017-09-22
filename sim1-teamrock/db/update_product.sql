4d2d5f5251f837f5f3c442af95c912dcf755262d
marinablack1992 <marinablack1992@gmail.com>
2017-09-22 14:09:57 -0600

    router file fixed maybe

---
+++ sim1-teamrock/db/update_product.sql
@@ -0,0 +1,3 @@
+UPDATE Products
+SET Price = $3, Name = $2
+WHERE ID = $1;
---
+++ sim1-teamrock/src/router.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import App from './App.js';
+import ShelfA from './components/ShelfA/ShelfA.js'
+import Bin1 from './components/ShelfA/BInsA/Bin1.js'
+import Bin2 from './components/ShelfA/BInsA/Bin2.js'
+import Bin3 from './components/ShelfA/BInsA/Bin3.js'
+import Bin4 from './components/ShelfA/BInsA/Bin4.js'
+import Bin5 from './components/ShelfA/BInsA/Bin5.js'
+
+export default (
+        <Switch>
+            <Route component={App} path='/' exact />
+            <Route component={ShelfA} path='/bins/A' />
+            <Route component={Bin1} path='/bin/A1' />
+            <Route component={Bin2} path='/bin/A2' />
+            <Route component={Bin3} path='/bin/A3' />
+            <Route component={Bin4} path='/bin/A4' />
+            <Route component={Bin5} path='/bin/A5' />
+        </Switch>
+)
